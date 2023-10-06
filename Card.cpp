00a414e5d9888c2d2cbf8adbf7ad1e3d66a3fd65
JeffordShau <shauj@umich.edu>
2023-10-05 21:33:48 -0400

    progress on card and pack

--- Card.cpp
+++ Card.cpp
@@ -92,14 +92,216 @@ std::istream & operator>>(std::istream &is, Suit &suit) {
 /////////////// Write your implementation for Card below ///////////////
 
 
-// NOTE: We HIGHLY recommend you check out the operator overloading
-// tutorial in the project spec before implementing
-// the following operator overload functions:
-//   operator<<
-//   operator>>
-//   operator<
-//   operator<=
-//   operator>
-//   operator>=
-//   operator==
-//   operator!=
+//EFFECTS Initializes Card to the Two of Spades
+Card::Card() {
+  rank = string_to_rank("TWO");
+  suit = string_to_suit("SPADES");
+} 
+
+//EFFECTS Initializes Card to specified rank and suit
+Card::Card(Rank rank_in, Suit suit_in) {
+  rank = rank_in;
+  suit = suit_in;
+}
+
+//EFFECTS Returns the rank
+Rank Card::get_rank() const {
+  return rank;
+}
+
+//EFFECTS Returns the suit.  Does not consider trump.
+Suit Card::get_suit() const {
+  return suit;
+}
+
+//EFFECTS Returns the suit
+//HINT: the left bower is the trump suit!
+Suit Card::get_suit(Suit trump) const {
+  if (rank == 9 && (suit == trump - 2 || suit == trump + 2)) {
+    return trump;   // when it is a Left Bower treat its suit as trump suit to compare.
+  }
+  return suit;   
+}
+
+//EFFECTS Returns true if card is a face card (Jack, Queen, King or Ace)
+bool Card::is_face_or_ace() const {
+  return rank >= 9;
+}
+
+//EFFECTS Returns true if card is the Jack of the trump suit
+bool Card::is_right_bower(Suit trump) const {
+  return rank == 9 && suit == trump;
+}
+
+//EFFECTS Returns true if card is the Jack of the next suit
+bool Card::is_left_bower(Suit trump) const {
+  return rank == 9 && (suit == trump - 2 || suit == trump + 2);
+
+}
+
+//EFFECTS Returns true if the card is a trump card.  All cards of the trump
+// suit are trump cards.  The left bower is also a trump card.
+bool Card::is_trump(Suit trump) const {
+  return get_suit(trump) == trump;
+}
+
+//EFFECTS Prints Card to stream, for example "Two of Spades"
+std::ostream & operator<<(std::ostream &os, const Card &card) {
+  os << RANK_NAMES[card.get_rank()] << " of " << SUIT_NAMES[card.get_suit()];
+  return os;
+}
+
+//EFFECTS Reads a Card from a stream in the format "Two of Spades"
+//NOTE The Card class declares this operator>> "friend" function,
+//     which means it is allowed to access card.rank and card.suit.
+std::istream & operator>>(std::istream &is, Card &card) {
+  string rank;
+  string junk;
+  string suit;
+  is >> rank >> junk >> suit;
+  card.rank = string_to_rank(rank);
+  card.suit = string_to_suit(suit);
+  return is;
+}
+
+//EFFECTS Returns true if lhs is lower value than rhs.
+//  Does not consider trump.
+bool operator<(const Card &lhs, const Card &rhs) {
+  if (lhs.get_rank() < rhs.get_rank()) {
+    return true;
+  }
+  else if (lhs.get_rank() > rhs.get_rank()) {
+    return false;
+  }
+  else {
+    return lhs.get_suit() < rhs.get_suit();
+  }
+}
+
+//EFFECTS Returns true if lhs is lower value than rhs or the same card as rhs.
+//  Does not consider trump.
+bool operator<=(const Card &lhs, const Card &rhs) {
+  if (lhs.get_rank() < rhs.get_rank()) {
+    return true;
+  }
+  else if (lhs.get_rank() > rhs.get_rank()) {
+    return false;
+  }
+  else {
+    return lhs.get_suit() <= rhs.get_suit();
+  }
+}
+
+//EFFECTS Returns true if lhs is higher value than rhs.
+//  Does not consider trump.
+bool operator>(const Card &lhs, const Card &rhs) {
+  if (lhs.get_rank() > rhs.get_rank()) {
+    return true;
+  }
+  else if (lhs.get_rank() < rhs.get_rank()) {
+    return false;
+  }
+  else {
+    return lhs.get_suit() > rhs.get_suit();
+  }
+}
+
+//EFFECTS Returns true if lhs is higher value than rhs or the same card as rhs.
+//  Does not consider trump.
+bool operator>=(const Card &lhs, const Card &rhs) {
+  if (lhs.get_rank() > rhs.get_rank()) {
+    return true;
+  }
+  else if (lhs.get_rank() < rhs.get_rank()) {
+    return false;
+  }
+  else {
+    return lhs.get_suit() >= rhs.get_suit();
+  }
+}
+
+//EFFECTS Returns true if lhs is same card as rhs.
+//  Does not consider trump.
+bool operator==(const Card &lhs, const Card &rhs) {
+  return lhs.get_rank() == rhs.get_rank() && lhs.get_suit() == rhs.get_suit();
+}
+
+//EFFECTS Returns true if lhs is not the same card as rhs.
+//  Does not consider trump.
+bool operator!=(const Card &lhs, const Card &rhs) {
+  return lhs.get_rank() != rhs.get_rank() || lhs.get_suit() != rhs.get_suit();
+}
+
+//EFFECTS returns the next suit, which is the suit of the same color
+Suit Suit_next(Suit suit) {
+  if (suit <= 1) {
+    return static_cast<Suit>(suit + 2);
+  }
+  else {
+    return static_cast<Suit>(suit - 2);
+  }
+}
+
+//EFFECTS Returns true if a is lower value than b.  Uses trump to determine
+// order, as described in the spec.
+bool Card_less(const Card &a, const Card &b, Suit trump) {
+  if (!(a.is_trump(trump)) && b.is_trump(trump)) {
+    return true;
+  }
+  else if (a.is_trump(trump) && !(b.is_trump(trump))) {
+    return false;
+  }
+  else if (a.is_trump(trump) && b.is_trump(trump)) {
+    if (!(a.is_right_bower(trump)) && b.is_right_bower(trump)) {
+      return true;
+    }
+    else if (a.is_right_bower(trump) && !(b.is_right_bower(trump))) {
+      return false;
+    }
+    else if (!(a.is_left_bower(trump)) && b.is_left_bower(trump)) {
+      return false;
+    }
+    else if (a.is_left_bower(trump) && !(b.is_left_bower(trump))) {
+      return false;
+    }
+    return a < b;
+  }
+  else {
+    return a < b;
+  }
+}
+
+//EFFECTS Returns true if a is lower value than b.  Uses both the trump suit
+//  and the suit led to determine order, as described in the spec.
+bool Card_less(const Card &a, const Card &b, const Card &led_card, Suit trump) {
+  if (!(a.is_trump(trump)) && b.is_trump(trump)) {
+    return true;
+  }
+  else if (a.is_trump(trump) && !(b.is_trump(trump))) {
+    return false;
+  }
+  else if (a.is_trump(trump) && b.is_trump(trump)) {
+    if (!(a.is_right_bower(trump)) && b.is_right_bower(trump)) {
+      return true;
+    }
+    else if (a.is_right_bower(trump) && !(b.is_right_bower(trump))) {
+      return false;
+    }
+    else if (!(a.is_left_bower(trump)) && b.is_left_bower(trump)) {
+      return false;
+    }
+    else if (a.is_left_bower(trump) && !(b.is_left_bower(trump))) {
+      return false;
+    }
+    return a < b;
+  }
+  else if (a.get_suit() != led_card.get_suit() && b.get_suit() == led_card.get_suit()) {
+    return true;
+  }
+  else if (a.get_suit() == led_card.get_suit() && b.get_suit() != led_card.get_suit()) {
+    return false;
+  }
+  else {
+    return a < b;
+  }
+}
---
+++ Pack.cpp
@@ -0,0 +1,99 @@
+#include "Pack.hpp"
+#include "Card.cpp"
+#include <string>
+#include <cstring>
+#include <cassert>
+#include <array>
+#include <iostream>
+
+
+// EFFECTS: Initializes the Pack to be in the following standard order:
+//          the cards of the lowest suit arranged from lowest rank to
+//          highest rank, followed by the cards of the next lowest suit
+//          in order from lowest to highest rank, and so on.
+// NOTE: The standard order is the same as that in pack.in.
+// NOTE: Do NOT use pack.in in your implementation of this function
+Pack::Pack(){
+
+    int index = 0;
+
+    for(int s = DIAMONDS; s >= SPADES; s--){
+        for(int r = 7; r < 13; r++){
+            cards[index++] = Card(static_cast<Rank>(r), static_cast<Suit>(s));
+        }
+    }
+
+    next = 0;
+}
+
+// REQUIRES: pack_input contains a representation of a Pack in the
+//           format required by the project specification
+// MODIFIES: pack_input
+// EFFECTS: Initializes Pack by reading from pack_input.
+Pack::Pack(std::istream& pack_input){
+    string rank;
+    string junk;
+    string suit;
+
+    int index = 0;
+
+    while(pack_input >> rank >> junk >> suit){
+        cards[index++] = Card(string_to_rank(rank), string_to_suit(suit));
+    }
+
+    next = 0;
+}
+
+// REQUIRES: cards remain in the Pack
+// EFFECTS: Returns the next card in the pack and increments the next index
+Card Pack::deal_one(){
+    Card *next_card = &cards[next];
+    ++next;
+    return *next_card;
+}
+
+// EFFECTS: Resets next index to first card in the Pack
+void Pack::reset(){
+    next = 0;
+}
+
+// EFFECTS: Shuffles the Pack and resets the next index. This
+//          performs an in shuffle seven times. See
+//          https://en.wikipedia.org/wiki/In_shuffle.
+void Pack::shuffle(){
+
+    const int HALF = PACK_SIZE / 2; // HALF = 12
+    const int ROUND_NUM = 7;        //ROUND_NUM = 7
+
+    Card *half_1 = new Card[HALF];
+    Card *half_2 = new Card[HALF];
+
+    for(int round = 0; round < ROUND_NUM; round++) { // we will shuffle for 7 rounds
+
+        for(int i = 0; i < PACK_SIZE; i++){  // we split the 24 cards into two pack
+            if(i < HALF){
+                half_1[i] = cards[i];
+            }
+            else{
+                half_2[i - HALF] = cards[i];
+            }
+        }
+
+        for(int i = 0; i < HALF; i++) {
+            cards[2 * i] = half_2[i];
+            cards[2 * i + 1] = half_1[i];
+        }
+
+       
+    }
+
+    delete[] half_1;
+    delete[] half_2;
+    next = 0;// EFFECTS: returns true if there are no more cards left in the pack
+    
+
+}
+
+bool Pack::empty() const{
+    return next == 24;
+}
